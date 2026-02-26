cb84a035396fbb908ac05c1753ff0db6f0002404
annalefarova <annalefarova@gmail.com>
2026-02-26 16:03:25 +0000

    Refactor CreatePrescriptionRequest

--- src/main/java/com/kneu/medications/dto/CreatePrescriptionRequest.java
+++ src/main/java/com/kneu/medications/dto/CreatePrescriptionRequest.java
@@ -1,10 +1,8 @@
 package com.kneu.medications.dto;
 
-import java.util.Optional;
-
 public record CreatePrescriptionRequest(
         String medicationId,
         String intakeTime,
-        Optional<String> label
+        String label
 ) {
 }
